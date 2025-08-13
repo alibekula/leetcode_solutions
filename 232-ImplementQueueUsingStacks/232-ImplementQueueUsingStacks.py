1e04b78733f1bd96ca06cad24fdf9ef393c22b05
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-13 16:58:21 +0600

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.py
@@ -0,0 +1,45 @@
+# Last updated: 13.08.2025, 16:58:21
+class MyQueue(object):
+    from collections import deque
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.queue.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.queue.popleft()
+        
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.queue[0]
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        if not self.queue:
+            return True
+        
+        return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
