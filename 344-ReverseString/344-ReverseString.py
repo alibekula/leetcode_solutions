c501ea5267c67eeed2661d8a83bb33c2250e2624
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-13 16:57:46 +0600

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.py
@@ -0,0 +1,7 @@
+# Last updated: 13.08.2025, 16:57:45
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = reversed(s)
