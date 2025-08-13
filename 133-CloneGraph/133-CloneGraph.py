31bcb858facca98d98b23584b2702249c0692d8b
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-13 16:59:28 +0600

    Create 133-CloneGraph

---
+++ 133-CloneGraph/133-CloneGraph.py
@@ -0,0 +1,33 @@
+# Last updated: 13.08.2025, 16:59:27
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
+        if not node:
+            return None
+            
+        visited = {}
+
+        def inner(node):
+            if node in visited:
+                return visited[node]
+
+            copy = Node(node.val)
+            visited[node] = copy
+
+            for n in node.neighbors:
+                copy.neighbors.append(inner(n))
+        
+            return copy
+        
+        
+
+        return inner(node)
