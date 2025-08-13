2fc8aa431fac21b507d0c439e6835c6dadf6b632
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-13 17:00:24 +0600

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -0,0 +1,19 @@
+# Last updated: 13.08.2025, 17:00:23
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n == 1:
+            return 1
+        
+        if n == 2:
+            return 2
+            
+        lst= [0] * (n+1)
+        lst[0] = 0
+        lst[1] = 1
+        lst[2] = 2
+
+        for i in range(3, n+1):
+            lst[i] = lst[i-1] + lst[i-2]
+
+        return lst[n]
