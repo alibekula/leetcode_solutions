182fc2371a2eccd1bc2d5ef220ec67309f10d933
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-13 16:59:00 +0600

    Create 179-LargestNumber

---
+++ 179-LargestNumber/179-LargestNumber.py
@@ -0,0 +1,8 @@
+# Last updated: 13.08.2025, 16:58:59
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str, nums))
+
+        nums.sort(key=lambda x: x*10, reverse=True)
+
+        return ''.join(nums) if nums[0] != "0" else '0' 
