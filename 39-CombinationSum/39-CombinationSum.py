2759495ede90185ba769f0d0443268ce914573eb
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-19 03:50:44 +0600

    File updated at 19.08.2025, 03:50:44

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,17 +1,18 @@
-# Last updated: 19.08.2025, 03:43:23
+# Last updated: 19.08.2025, 03:50:44
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
+    def firstBadVersion(self, n: int) -> int:
+        
+        l, r = 0, n-1
 
         while l <= r:
             mid = (l+r)//2
 
-            if nums[mid] == target:
-                return mid
-            
-            if nums[mid] > target:
-                r = mid - 1
-            else:
+            if not isBadVersion(mid):
                 l = mid + 1
+            else:
+                r = mid - 1
         
-        return -1
+        return r + 1
