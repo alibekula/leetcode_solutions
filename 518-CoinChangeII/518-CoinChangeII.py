d7cd59a85a09c1e2d9e7920036844c14437fe0ec
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-20 01:06:00 +0600

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -0,0 +1,20 @@
+# Last updated: 20.08.2025, 01:05:38
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        if amount == 0:
+            return 1
+        
+        coins.sort()
+        dp = [0 for _ in range(amount + 1)]
+        dp[0] = 1
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i-coin]
+ 
+
+        return dp[amount]
+
+
+                
