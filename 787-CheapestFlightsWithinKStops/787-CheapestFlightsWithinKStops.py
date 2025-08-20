e5502303c3d4b3db905bbdf8b29d53b19c1ea4ca
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-21 02:18:17 +0600

    File updated at 21.08.2025, 02:18:17

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.py
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.py
@@ -1,33 +1,60 @@
-# Last updated: 21.08.2025, 01:32:41
-from collections import defaultdict
-
+# Last updated: 21.08.2025, 02:18:17
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def _find(self, node):
+        if self.roots[node] == node:
+            return node
         
-        graph = defaultdict(list)
+        self.roots[node] = self._find(self.roots[node])
+        return self.roots[node]
+    
+    def _union(self, node1, node2):
+        root1 = self._find(node1)
+        root2 = self._find(node2)
 
-        for start, end, price in flights:
-            graph[start].append([end, price])
+        if root1 != root2:
+            self.roots[root2] = root1
+            return False
         
-        heap = []
-        flights_count = {}
-        heapq.heappush(heap, [0, src, 0])
+        return True
+    
+    def _calc_dist(self, node1, node2):
+        x1, y1 = node1
+        x2, y2 = node2
 
-        while heap:
-            curr, node, stops = heapq.heappop(heap)
+        return abs(x1 - x2) + abs(y1 - y2)
 
-            if node in flights_count and flights_count[node] <= stops:
-                continue
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        points = [tuple(i) for i in points]
+        visited = [False for _ in range(n)]
+        k = n-1
+
+        self.roots = {key: value for key, value in zip(points, points)}
+
+        edges = []
+
+        for i in range(n):
+            p1 = points[i]
+            for j in range(i+1, n):
+                p2 = points[j]
+                edges.append([self._calc_dist(p1, p2), p1, p2])
+        
+        edges.sort()
+        ans = 0
+
+        for edge in edges:
+            dist, p1, p2 = edge
+
+            if not self._union(p1, p2):
+                ans += dist
+                k -= 1
             
-            flights_count[node] = stops
+            if k == 0:
+                return ans
+        
+        return ans
 
-            if node == dst:
-                return curr
-            
-            if stops <= k:
-                for nei, w in graph[node]:
-                    heapq.heappush(heap, [curr+w, nei, stops+1])
         
-        return -1
-        
+
             
+
