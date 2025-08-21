045cab9b1a14d3efd73de6e47989c7afb0ec2636
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-21 10:12:19 +0600

    File updated at 21.08.2025, 10:12:19

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,18 +1,31 @@
-# Last updated: 21.08.2025, 08:58:08
+# Last updated: 21.08.2025, 10:12:19
+from collections import Counter
 class Solution:
-    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+    def partition(self, s: str) -> list[[str]]:
         
-        n, m = len(nums1), len(nums2)
+        n = len(s)
+        dp = [[False for _ in range(n)] for _ in range(n)]
 
-        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        max_subseq = 0
+        for i in range(n):
+            dp[i][i] = True
+            for j in range(i-1, -1, -1):
+                if s[i] == s[j] and (i-j < 2 or dp[i-1][j+1]):
+                    dp[i][j] = True
 
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if nums1[i-1] == nums2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
-                
-                max_subseq = max(dp[i][j], max_subseq)
-        
-        return max_subseq
-                
+        ans = []
+        curr = []
+
+        def dfs(i):
+            nonlocal n, s
+            if i >= n:
+                ans.append(curr[:])
+                return
+
+            for j in range(i, n):
+                if dp[j][i]:
+                    curr.append(s[i:j+1])
+                    dfs(j+1)           
+                    curr.pop() 
+
+        dfs(0)
+        return ans
