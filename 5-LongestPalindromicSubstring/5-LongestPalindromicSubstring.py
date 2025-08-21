169c01854d0f610d302e80092175aaeafa39313c
alibekula <146336542+alibekula@users.noreply.github.com>
2025-08-21 07:06:54 +0600

    File updated at 21.08.2025, 07:06:54

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,22 +1,26 @@
-# Last updated: 21.08.2025, 06:59:56
+# Last updated: 21.08.2025, 07:06:54
 class Solution:
+    def expand_from_center(self, i, j):
+
+        l, r = i, j
+
+        while (r < self.n and l >= 0) and (self.s[l] == self.s[r]):
+            l -= 1
+            r += 1
+        
+        return [l+1, r-1]
+
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
+        self.n = len(s)
+        self.s = s
+        max_len = [0, 0]
+        
+        for i in range(self.n):
 
-        if n == 0:
-            return ''
+            odd = self.expand_from_center(i, i)
+            even = self.expand_from_center(i, i+1)
 
-        dp = [[False for _ in range(n)] for _ in range(n)]
-        max_str = s[0]
+            max_len = max(odd, even, max_len, key=lambda x: x[1] - x[0])
+        
+        return s[max_len[0]:max_len[1]+1]
 
-        for i in range(n):
-            dp[i][i] = True
-
-            for j in range(i-1, -1, -1):
-                if s[i] == s[j] and (dp[j+1][i-1] or i-j < 2):
-                    dp[j][i] = True
-
-                    if i - j + 1 > len(max_str):
-                        max_str = s[j: i+1]
-            
-        return max_str
