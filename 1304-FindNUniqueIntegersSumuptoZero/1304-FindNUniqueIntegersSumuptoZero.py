99d0d10251c9dbba8e6d6256eef0edabafae7738
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-07 21:06:41 +0600

    File updated at 07.09.2025, 21:06:40

--- 1304-FindNUniqueIntegersSumuptoZero/1304-FindNUniqueIntegersSumuptoZero.py
+++ 1304-FindNUniqueIntegersSumuptoZero/1304-FindNUniqueIntegersSumuptoZero.py
@@ -1,16 +1,25 @@
-# Last updated: 07.09.2025, 20:20:49
-memo = {1:[0]}
-for i in range(1, 1000):
-    curr = []
-    for j in range(1, i+2):
-        curr.append(j)
-
-    total = sum(curr) - curr[-1]
-    curr[-1] = -total
-    memo[i+1] = curr[:]
+# Last updated: 07.09.2025, 21:06:40
 
 
 class Solution:
+    
+    def __init__(self):
+        self.memo = {}
+
     def sumZero(self, n: int) -> List[int]:
-        return memo[n]
+        ans = []
+
+        if n in self.memo:
+            return self.memo[n]
+        
+        if n % 2 == 1:
+            ans.append(0)
+        
+        for i in range(1, n // 2+1):
+            ans.append(i)
+            ans.append(-i)
+        
+        self.memo[n] = ans
+
+        return ans
         
