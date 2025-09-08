b081f1bf12f4617a3fee8c98256d5372ab54b2f4
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-08 20:21:20 +0600

    File updated at 08.09.2025, 20:21:20

--- 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.py
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.py
@@ -1,34 +1,30 @@
-# Last updated: 08.09.2025, 20:19:46
+# Last updated: 08.09.2025, 20:21:20
+from collections import deque
+
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights: 
+            return []
         
-        pacific = set()
-        atlantic = set()
         n, m = len(heights), len(heights[0])
-
-        def dfs(i, j, reachable):
-            nonlocal n, m
-            reachable.add((i, j))
-            directions = [(0,1), (1,0), (-1,0), (0,-1)]
-
-            for x, y in directions:
-                di = i + x
-                dj = j + y
-
-                if (di, dj) in reachable or not (0<=di<n and 0<=dj<m):
-                    continue
-                
-                if heights[i][j] > heights[di][dj]:
-                    continue
-
-                dfs(di, dj, reachable)
         
-        for i in range(n):
-            dfs(i, 0, pacific)
-            dfs(i, m-1, atlantic)
+        def bfs(starts):
+            visited = set(starts)
+            q = deque(starts)
+            while q:
+                i, j = q.popleft()
+                for di, dj in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    ni, nj = i + di, j + dj
+                    if 0 <= ni < n and 0 <= nj < m and (ni, nj) not in visited:
+                        if heights[ni][nj] >= heights[i][j]:
+                            visited.add((ni, nj))
+                            q.append((ni, nj))
+            return visited
         
-        for j in range(m):
-            dfs(0, j, pacific)
-            dfs(n-1, j, atlantic)
+        pacific_starts = [(0, j) for j in range(m)] + [(i, 0) for i in range(n)]
+        atlantic_starts = [(n-1, j) for j in range(m)] + [(i, m-1) for i in range(n)]
         
-        return list(pacific.intersection(atlantic))
+        pacific = bfs(pacific_starts)
+        atlantic = bfs(atlantic_starts)
+        
+        return list(map(list, pacific & atlantic))
