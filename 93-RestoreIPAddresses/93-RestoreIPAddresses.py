ac43677861ffae6b1a5809e172b1bfefdc5d3688
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-10 03:34:51 +0600

    Create 93-RestoreIPAddresses

---
+++ 93-RestoreIPAddresses/93-RestoreIPAddresses.py
@@ -0,0 +1,62 @@
+# Last updated: 10.09.2025, 03:34:50
+class Solution:
+
+    def isValid(self, string):
+
+        if not string:
+            return False
+
+        num =int(string)
+
+        if 0 > num or num > 255:
+            return False
+
+        if len(string) > 1 and string[0] == '0':
+            return False
+
+        return True
+
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        n = len(s)
+
+        if n > 12 or n < 4:
+            return []
+
+        ans = []
+
+        def backtrack(i, path):
+            if len(path) > 4:
+                return
+
+            if i >= n and len(path) == 4:
+                ans.append('.'.join(path))
+                return
+
+            string = ''
+            candidates = []
+
+            for j in range(i, i + 3):
+                if j < n:
+                    string += s[j]
+                    next_idx = j + 1
+
+                    if self.isValid(string):
+                        candidates.append([string, next_idx])
+            
+            for candidate, idx in candidates:
+                path.append(candidate)
+
+                backtrack(idx, path)
+
+                path.pop()
+        
+        backtrack(0, [])
+        
+        return ans
+
+
+                
+            
+
+            
+
