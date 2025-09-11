203855ef33134155472e5f8fc23343c4a03e45f6
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-12 03:06:51 +0600

    Create 2887-SortVowelsInAString

---
+++ 2887-SortVowelsInAString/2887-SortVowelsInAString.py
@@ -0,0 +1,22 @@
+# Last updated: 12.09.2025, 03:06:51
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels = set('aeiouAEIOU')
+        order = "AEIOUaeiou"  
+        freq = {ch: 0 for ch in order}
+        
+        lst = list(s)
+        
+        for ch in s:
+            if ch in vowels:
+                freq[ch] += 1
+        
+        idx = 0  
+        for i, ch in enumerate(lst):
+            if ch in vowels:
+                while freq[order[idx]] == 0:
+                    idx += 1
+                lst[i] = order[idx]
+                freq[order[idx]] -= 1
+        
+        return ''.join(lst)
