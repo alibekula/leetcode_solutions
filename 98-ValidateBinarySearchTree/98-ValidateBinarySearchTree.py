52f6b84dafdd65289136e81ac65443bd93153d73
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-12 15:52:00 +0600

    File updated at 12.09.2025, 15:52:00

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 12.09.2025, 15:47:44
+# Last updated: 12.09.2025, 15:52:00
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,20 +7,21 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        prev = None 
-
-        def dfs(node):
-            nonlocal prev
+        
+        def dfs(node, mm=float('-inf'), mx=float('inf')):
             if not node:
                 return True
             
-            if not dfs(node.left):
+            if not dfs(node.left, mm = mm, mx= node.val):
                 return False
             
-            if prev is not None and prev >= node.val:
+            if not (mm < node.val < mx):
                 return False
-            prev = node.val
-            return dfs(node.right)
+                
+            if not dfs(node.right, mm=node.val, mx=mx):
+                return False
+            
+            return True
         
         return dfs(root)
             
