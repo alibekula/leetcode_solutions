531d1dc14495c93957ccd0d19afd64fcba6a0414
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-16 03:17:06 +0600

    File updated at 16.09.2025, 03:17:05

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,23 +1,20 @@
-# Last updated: 17.08.2025, 20:35:55
+# Last updated: 16.09.2025, 03:17:05
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = {len(s):1}
+        
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[0] = 1
+        # 12
+        # 0 1 
+        for i in range(n):
 
-        def search(curr):
-            if curr in dp:
-                return dp[curr]
-            if curr >= len(s):
-                return 
-            if s[curr] == '0':
+            if s[i] =='0' and (i ==0 or s[i-1] not in ('1', '2')):
                 return 0
 
-            res = search(curr+1)
+            if i > 0 and ((s[i-1] == '1' and s[i] in '987654321') or (s[i-1] == '2' and s[i] in '123456')) and (i +1 >= n or s[i+1] != '0'):
+                dp[i+1] = dp[i] + dp[i-1]
+            else:
+                dp[i+1] = dp[i]
 
-            if curr+1 < len(s) and ((s[curr] in '2' and s[curr+1] in '0123456') or (s[curr] == '1')):
-                res += search(curr+2)
-            
-            dp[curr] = res
-
-            return res
-        
-        return search(0)
+        return dp[n]   
