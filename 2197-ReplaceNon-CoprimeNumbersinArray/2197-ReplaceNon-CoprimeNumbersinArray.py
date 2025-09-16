a5088f774a377989d0234be55e4860498bf35ac6
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-16 08:53:03 +0600

    Create 2197-ReplaceNon-CoprimeNumbersinArray

---
+++ 2197-ReplaceNon-CoprimeNumbersinArray/2197-ReplaceNon-CoprimeNumbersinArray.py
@@ -0,0 +1,31 @@
+# Last updated: 16.09.2025, 08:53:02
+class Solution:
+
+    def LCM(self, a, b):
+        gcd = self.GCD(a, b)
+
+        return (a * b)//gcd 
+    
+    def GCD(self, a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
+
+
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        
+        stack = []
+        
+        for num in nums:
+            stack.append(num)
+            while len(stack) >= 2:
+                g = self.GCD(stack[-1], stack[-2])
+                if g == 1:
+                    break
+                lcm = self.LCM(stack[-1],  stack[-2])
+                stack.pop()
+                stack.pop()
+                stack.append(lcm)
+        
+        return stack
+        
