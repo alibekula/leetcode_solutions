828fed140e306552a6d3f4ea2d41f20571cdfdcb
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-22 21:40:33 +0600

    Create 3005-CountElementsWithMaximumFrequency

---
+++ 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.py
@@ -0,0 +1,16 @@
+# Last updated: 22.09.2025, 21:40:30
+from collections import Counter
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        
+        counter = Counter(nums)
+        max_count_key = max(counter, key=lambda x: counter[x])
+        max_count = counter[max_count_key]
+
+        n = 0
+
+        for val in counter.values():
+            if val == max_count:
+                n += 1
+        
+        return n * max_count
