e63a409c2c762cfce1d47b0fa03a6ea00c6cad76
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-23 16:40:13 +0600

    Create 3678-DesignTaskManager

---
+++ 3678-DesignTaskManager/3678-DesignTaskManager.py
@@ -0,0 +1,34 @@
+# Last updated: 23.09.2025, 16:40:13
+import heapq
+from typing import List
+
+class TaskManager:
+    def __init__(self, tasks: List[List[int]]):
+        self.heap = []  # (-priority, -taskId, taskId, userId)
+        self.task_info = {}  # taskId -> (priority, userId)
+
+        for userId, taskId, priority in tasks:
+            self.add(userId, taskId, priority)
+
+    def add(self, userId: int, taskId: int, priority: int) -> None:
+        self.task_info[taskId] = (priority, userId)
+        heapq.heappush(self.heap, (-priority, -taskId, taskId, userId))
+
+    def edit(self, taskId: int, newPriority: int) -> None:
+        _, userId = self.task_info[taskId]
+        self.task_info[taskId] = (newPriority, userId)
+        heapq.heappush(self.heap, (-newPriority, -taskId, taskId, userId))
+
+    def rmv(self, taskId: int) -> None:
+        if taskId in self.task_info:
+            del self.task_info[taskId]
+
+    def execTop(self) -> int:
+        while self.heap:
+            priority, negTaskId, taskId, userId = heapq.heappop(self.heap)
+            if taskId in self.task_info:
+                curPriority, curUser = self.task_info[taskId]
+                if curPriority == -priority and curUser == userId:
+                    del self.task_info[taskId]
+                    return userId
+        return -1
