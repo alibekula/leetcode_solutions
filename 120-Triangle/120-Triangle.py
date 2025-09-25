eaa98846b1bb38e5a2059c88810bd4fc4e73410b
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-25 13:50:47 +0600

    File updated at 25.09.2025, 13:50:42

--- 120-Triangle/120-Triangle.py
+++ 120-Triangle/120-Triangle.py
@@ -1,10 +1,11 @@
-# Last updated: 13.08.2025, 16:59:38
+# Last updated: 25.09.2025, 13:50:42
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         dp = triangle[-1][:]
+        n = len(dp)
 
-        for level in range(len(triangle)-2,-1, -1):
+        for level in range(n-2, -1, -1):
             for idx in range(len(triangle[level])):
                 dp[idx] = triangle[level][idx] + min(dp[idx], dp[idx+1])
-
-        return dp[0] 
+        
+        return dp[0]
