137879741472f7a5e1fd3ec15332e2f848bd84a7
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-27 06:26:05 +0600

    File updated at 27.09.2025, 06:25:53

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,30 +1,41 @@
-# Last updated: 27.09.2025, 05:23:55
-class Solution:
-    def compress(self, chars: List[str]) -> int:
+# Last updated: 27.09.2025, 06:25:53
+class MinStack:
+# 1, 2, 2, 2, -5, 8,  1
+# 1, 1, 1, 1, -5, -5, -5
+    def __init__(self):
+        self.min_stack = []
+        self.stack = []
+        self.curr_min = None
+    def push(self, val: int) -> None:
 
-        if not chars:
-            return 0
-
-        chars.append('<eol>')
-        n = len(chars)
-        count = 1
-        p = 0
-        prev = chars[0]
-
-        for i in range(1, n):
-            curr = chars[i]
-
-            if prev == curr:
-                count += 1
-            else:
-                chars[p] = prev
-                p += 1
-                if count != 1:
-                    for num in str(count):
-                        chars[p] = num
-                        p += 1
-                
-                prev = curr
-                count = 1
+        if self.curr_min is None:
+            self.curr_min = val
+        elif val < self.curr_min:
+            self.curr_min = val
         
-        return p
+        self.stack.append(val)
+        self.min_stack.append(self.curr_min)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        minimum = self.min_stack.pop()
+        if self.curr_min == minimum:
+            self.curr_min = self.min_stack[-1] if self.min_stack else None 
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
