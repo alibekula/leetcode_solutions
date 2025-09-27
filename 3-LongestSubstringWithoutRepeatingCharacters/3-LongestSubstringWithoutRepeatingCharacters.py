f24f77914905a4af9c173a6a63526613398de490
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-27 06:45:57 +0600

    File updated at 27.09.2025, 06:45:45

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,41 +1,17 @@
-# Last updated: 27.09.2025, 06:25:53
-class MinStack:
-# 1, 2, 2, 2, -5, 8,  1
-# 1, 1, 1, 1, -5, -5, -5
-    def __init__(self):
-        self.min_stack = []
-        self.stack = []
-        self.curr_min = None
-    def push(self, val: int) -> None:
-
-        if self.curr_min is None:
-            self.curr_min = val
-        elif val < self.curr_min:
-            self.curr_min = val
+# Last updated: 27.09.2025, 06:45:45
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
         
-        self.stack.append(val)
-        self.min_stack.append(self.curr_min)
-        
+        total = 0
+        curr = 0
+        prefix = {0:1}
 
-    def pop(self) -> None:
-        self.stack.pop()
-        minimum = self.min_stack.pop()
-        if self.curr_min == minimum:
-            self.curr_min = self.min_stack[-1] if self.min_stack else None 
+        for num in nums:
+            curr += num
 
+            if curr - k in prefix:
+                total += prefix[curr-k]
 
-    def top(self) -> int:
-        return self.stack[-1]
-        
+            prefix[curr] = prefix.get(curr, 0) + 1
 
-    def getMin(self) -> int:
-        return self.min_stack[-1]
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        return total
