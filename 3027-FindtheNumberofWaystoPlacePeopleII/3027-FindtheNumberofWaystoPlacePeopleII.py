9ad7186855dcea2e51e77b8b3630a1ba1cd84478
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-28 07:03:50 +0600

    File updated at 28.09.2025, 07:03:50

--- 3027-FindtheNumberofWaystoPlacePeopleII/3027-FindtheNumberofWaystoPlacePeopleII.py
+++ 3027-FindtheNumberofWaystoPlacePeopleII/3027-FindtheNumberofWaystoPlacePeopleII.py
@@ -1,15 +1,22 @@
-# Last updated: 28.09.2025, 06:32:03
+# Last updated: 28.09.2025, 07:03:50
 class Solution:
     def numberOfPairs(self, P: List[List[int]]) -> int:
-        P.sort(key=lambda p:(-p[0], p[1]))
-        ans, n=0, len(P)
-        for i in range(n-1):
-            y, yi=1<<31, P[i][1]
-            for j in range(i+1, n):
-                yj=P[j][1]
-                if y>yj>=yi:
-                    ans+=1
-                    y=yj
-                    if yi==yj: break
-        return ans
         
+        P.sort(key = lambda x: (-x[0], x[1]))
+
+        ans, n = 0, len(P)
+
+        for i in range(n-1):
+            y_max, y_min = float('inf'), P[i][1]
+
+            for j in range(i+1, n):
+                
+                y = P[j][1]
+
+                if y_max > y >= y_min:
+                    ans += 1
+                    y_max = y
+                    if y == y_min:
+                        break
+        
+        return ans
