e9cf9b6b1e209ea2ba8c95f62a198be82be36849
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-28 06:32:03 +0600

    File updated at 28.09.2025, 06:32:03

--- 3027-FindtheNumberofWaystoPlacePeopleII/3027-FindtheNumberofWaystoPlacePeopleII.py
+++ 3027-FindtheNumberofWaystoPlacePeopleII/3027-FindtheNumberofWaystoPlacePeopleII.py
@@ -1,59 +1,15 @@
-# Last updated: 28.09.2025, 06:30:49
+# Last updated: 28.09.2025, 06:32:03
 class Solution:
-    def numberOfPairs(self, points: List[List[int]]) -> int:
-        col = {}
-        row = {}
-        coordinates_map = {}
-
-        for point in points:
-            x, y = point
-            col[x] = 0
-            row[y] = 0
-
-        def map_keys_to_order(m):
-            sorted_keys = sorted(m.keys())
-            for idx, key in enumerate(sorted_keys):
-                m[key] = idx + 1
-
-        map_keys_to_order(col)
-        map_keys_to_order(row)
-        nc = len(col) + 1
-        nr = len(row) + 1
-        m = [[0] * nr for _ in range(nc)]
-        prefix_sum = [[0] * nr for _ in range(nc)]
-
-        for point in points:
-            x, y = point
-            c = col[x]
-            r = row[y]
-            coordinates_map[tuple(point)] = (c, r)
-            m[c][r] = 1
-
-        for i in range(1, nc):
-            for j in range(1, nr):
-                prefix_sum[i][j] = (
-                    prefix_sum[i - 1][j]
-                    + prefix_sum[i][j - 1]
-                    - prefix_sum[i - 1][j - 1]
-                    + m[i][j]
-                )
-
-        ans = 0
-        points.sort(key=lambda p: (p[0], -p[1]))
-        n = len(points)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if points[i][1] >= points[j][1]:
-                    c1, r1 = coordinates_map[tuple(points[i])]
-                    c2, r2 = coordinates_map[tuple(points[j])]
-                    cnt = (
-                        prefix_sum[c2][r1]
-                        - prefix_sum[c1 - 1][r1]
-                        - prefix_sum[c2][r2 - 1]
-                        + prefix_sum[c1 - 1][r2 - 1]
-                    )
-
-                    if cnt == 2:
-                        ans += 1
-
+    def numberOfPairs(self, P: List[List[int]]) -> int:
+        P.sort(key=lambda p:(-p[0], p[1]))
+        ans, n=0, len(P)
+        for i in range(n-1):
+            y, yi=1<<31, P[i][1]
+            for j in range(i+1, n):
+                yj=P[j][1]
+                if y>yj>=yi:
+                    ans+=1
+                    y=yj
+                    if yi==yj: break
         return ans
+        
