5aa58f418f72ec66083f7fbc74b843ecb281deff
alibekula <146336542+alibekula@users.noreply.github.com>
2025-09-28 10:16:10 +0600

    File updated at 28.09.2025, 10:16:10

--- 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).py
+++ 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).py
@@ -1,48 +1,22 @@
-# Last updated: 28.09.2025, 09:26:25
-from collections import defaultdict
-from random import choice
-class RandomizedSet:
+# Last updated: 28.09.2025, 10:16:10
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
-    def __init__(self):
-        self.dct = {} # key(value) -> value(idx)
-        self.lst = list()
+        if not intervals:
+            return []
+            
+        intervals.sort(key=lambda x: x[0])
 
-    def insert(self, val: int) -> bool:
+        n = len(intervals)
+        ans = [intervals[0]]
 
-        if val in self.dct:
-            return False
-        idx = len(self.lst)
+        for i in range(1, n):
+            start1, end1 = ans[-1]
+            start2, end2 = intervals[i]
 
-        self.dct[val] = idx
-        self.lst.append(val)
-        return True
-
-    def remove(self, val: int) -> bool:
+            if end1 >= start2:
+                ans[-1] = [start1, max(end1, end2)]
+            else:
+                ans.append([start2, end2])
         
-        if val not in self.dct:
-            return False
-        
-        idx = self.dct[val]
-        last_item = self.lst.pop()
-        del self.dct[val]
-
-        if not self.lst or idx == len(self.lst):
-            return True
-
-        self.lst[idx] = last_item
-        self.dct[last_item] = idx
-
-
-        return True
-        
-
-    def getRandom(self) -> int:
-        return choice(self.lst)
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        return ans
