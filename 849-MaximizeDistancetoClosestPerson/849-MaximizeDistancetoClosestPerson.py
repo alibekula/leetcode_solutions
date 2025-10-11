ee022f6602d64539b9fc0300772f84cf8ddf6f9b
alibekula <146336542+alibekula@users.noreply.github.com>
2025-10-11 20:53:17 +0600

    File updated at 11.10.2025, 20:53:16

--- 849-MaximizeDistancetoClosestPerson/849-MaximizeDistancetoClosestPerson.py
+++ 849-MaximizeDistancetoClosestPerson/849-MaximizeDistancetoClosestPerson.py
@@ -1,26 +1,29 @@
-# Last updated: 11.10.2025, 20:15:13
+# Last updated: 11.10.2025, 20:53:16
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
         
         n = len(seats)
-        r = 1
         length = 1
         # 0 0 0 0 1
         # 1 0 0 0 0
         # 1 0 0 0 1
         # 0 0 1 0 0 1 0 0 0 0 0 0 
 
-        while r < n:
-            l = r - 1
+        used = (i for i, seat in enumerate(seats) if seat == 1)
+        prev = None
+        future = next(used, None)
 
-            while r < n and seats[r] != 1:
-                r += 1
-
-            if r < n and seats[r] == 1 and seats[l] == 1:
-                length = max(length, (r - l) // 2)
+        for idx, seat in enumerate(seats):
+            if seat:
+                prev = idx
             else:
-                new_length = r - l if seats[l] == 0 else r - l -1
-                length = max(length, new_length)
-            r += 1
 
+                while future is not None and future < idx:
+                    future = next(used, None)
+
+                left = float('inf') if prev is None else idx - prev
+                right = float('inf') if future is None else future - idx
+
+                length = max(length, min(left, right))
+        
         return length
