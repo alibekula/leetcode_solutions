ef5f141234b852e996b4ca3c7eb0090c93958eb5
alibekula <146336542+alibekula@users.noreply.github.com>
2025-10-11 22:46:32 +0600

    File updated at 11.10.2025, 22:46:31

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,11 +1,29 @@
-# Last updated: 13.08.2025, 16:57:50
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')]*(amount+1)
-        dp[0] = 0
-        for coin in coins:
-            for k in range(coin, amount+1):
-                dp[k] = min(dp[k], dp[k-coin]+1)
-        print(dp)
-        return dp[amount] if dp[amount] != float('inf') else -1
+# Last updated: 11.10.2025, 22:46:31
+from collections import deque
 
+class Solution:
+    def coinChange(self, coins, amount):
+        if amount == 0:
+            return 0
+        
+        coins.sort()
+        dp = {c: 1 for c in coins}
+        queue = deque(coins)
+        
+        if min(coins) > amount:
+            return -1
+        if amount in dp:
+            return 1
+
+        while queue:
+            curr = queue.popleft()
+            for coin in coins:
+                new_amount = curr + coin
+                if new_amount > amount:
+                    break
+                if new_amount not in dp:
+                    dp[new_amount] = dp[curr] + 1
+                    if new_amount == amount:
+                        return dp[new_amount]
+                    queue.append(new_amount)
+        return -1
