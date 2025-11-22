c24bac4fd925507a481ce4031c3a00b27cbea2ce
alibekula <146336542+alibekula@users.noreply.github.com>
2025-11-22 21:04:06 +0600

    File updated at 22.11.2025, 21:04:05

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,18 +1,21 @@
-# Last updated: 22.11.2025, 20:55:35
-from collections import Counter
+# Last updated: 22.11.2025, 21:04:05
 class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         
-        counter = {key: 0 for key in range(1, len(nums)+1)}
-        ans = [-1, -1]
+        arr = sorted(nums)
+        hashmap = {}
+
+        rank = 0
+        n = len(nums)
+
+        for i in range(n):
+            if arr[i] not in hashmap:
+                hashmap[arr[i]] = rank
+            rank += 1
+
+        ans = []
 
         for num in nums:
-            counter[num] += 1
-
-        for key, value in counter.items():
-            if value == 2:
-                ans[0] = key
-            elif value == 0:
-                ans[1] = key
-
+            ans.append(hashmap[num])
+        
         return ans
