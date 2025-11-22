517fefe6ae5d2895d15c6e0bf410bd4d4aec8df6
alibekula <146336542+alibekula@users.noreply.github.com>
2025-11-22 23:40:03 +0600

    File updated at 22.11.2025, 23:40:02

--- 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.py
+++ 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.py
@@ -1,19 +1,26 @@
-# Last updated: 22.11.2025, 23:39:44
+# Last updated: 22.11.2025, 23:40:02
 class Solution:
-    def buildArray(self, target: List[int], n: int) -> List[str]:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         
-        p = 0
-        ans = []
+        inside = 0
+        stack = []
+        dct = {str(key): 0 for key in range(n)}
 
-        for i in range(1, n + 1):
-            if i == target[p]:
-                p += 1
-                ans.append('Push')
+        for log in logs:
+            idx0, op, ts = log.split(':')
+            ts = int(ts)
 
-                if p == len(target):
-                    break
+            if op == 'end':
+
+                idx1, start = stack.pop()
+                dct[idx0] += ts - start + 1
+
+                if stack:
+                    idx2 = stack[-1][0]
+                    dct[idx2] -= ts-start +1 
             else:
-                ans.append('Push')
-                ans.append('Pop')
+                stack.append([idx0, ts])
         
-        return ans
+        return list(dct.values())
+
+
