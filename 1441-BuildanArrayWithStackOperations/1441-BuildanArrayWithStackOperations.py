e043188a68f735b9195ae9668ace4c776ae6093f
alibekula <146336542+alibekula@users.noreply.github.com>
2025-11-22 21:26:16 +0600

    Create 1441-BuildanArrayWithStackOperations

---
+++ 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.py
@@ -0,0 +1,19 @@
+# Last updated: 22.11.2025, 21:26:15
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        
+        p = 0
+        ans = []
+
+        for i in range(1, n + 1):
+            if i == target[p]:
+                p += 1
+                ans.append('Push')
+
+                if p == len(target):
+                    break
+            else:
+                ans.append('Push')
+                ans.append('Pop')
+        
+        return ans
