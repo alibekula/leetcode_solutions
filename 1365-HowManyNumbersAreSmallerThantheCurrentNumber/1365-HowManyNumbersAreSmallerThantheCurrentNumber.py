8d100955ea46d08b5bbdb463e0b154d39a592a0b
alibekula <146336542+alibekula@users.noreply.github.com>
2025-11-22 21:08:46 +0600

    Create 1365-HowManyNumbersAreSmallerThantheCurrentNumber

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.py
@@ -0,0 +1,16 @@
+# Last updated: 22.11.2025, 21:08:45
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+        dct = {key: 0 for key in range(1, len(nums)+1)}
+
+        ans = []
+
+        for num in nums:
+            dct[num] += 1
+
+        for key, value in dct.items():
+            if value == 0:
+                ans.append(key)
+
+        return ans
