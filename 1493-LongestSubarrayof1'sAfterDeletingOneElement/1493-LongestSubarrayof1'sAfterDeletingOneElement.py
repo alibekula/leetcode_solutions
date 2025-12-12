89500f99cf9b2e5e0c630d5614b5674f00752953
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-12 21:28:37 +0600

    File updated at 12.12.2025, 21:28:37

--- 1493-LongestSubarrayof1'sAfterDeletingOneElement/1493-LongestSubarrayof1'sAfterDeletingOneElement.py
+++ 1493-LongestSubarrayof1'sAfterDeletingOneElement/1493-LongestSubarrayof1'sAfterDeletingOneElement.py
@@ -1,25 +1,25 @@
-# Last updated: 10.10.2025, 22:20:05
-class Solution:
-    def longestSubarray(self, nums: List[int]) -> int:
-        
-        n = len(nums)
-        l, r = 0, 0
-        zero = False
-        length = 0
-        # 1 0 1 1 1 0 0 1
-        while r < n:
-
-            while r < n and (nums[r] != 0 or not zero):
-                if nums[r] == 0:
-                    zero = True
-                length = max(length, r-l)
-                r += 1
-
-            
-            while l < r and zero:
-                if nums[l] == 0:
-                    zero = False
-                l += 1
-
-        return length
-
+# Last updated: 12.12.2025, 21:28:37
+1class Solution:
+2    def longestSubarray(self, nums: List[int]) -> int:
+3        longest = 0
+4
+5        l, r = 0, 0
+6        zeros = 0
+7
+8        while r < len(nums):
+9
+10            while r < len(nums) and (nums[r] != 0 or zeros == 0):
+11                if nums[r] == 0:
+12                    zeros += 1
+13                
+14                longest = max(longest, r-l)
+15                r += 1
+16            
+17            while l < r and zeros == 1:
+18                if nums[l] == 0:
+19                    zeros -= 1
+20                
+21                l += 1
+22        
+23        return longest
+24
