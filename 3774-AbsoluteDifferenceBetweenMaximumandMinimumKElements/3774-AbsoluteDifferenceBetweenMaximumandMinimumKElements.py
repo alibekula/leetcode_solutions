0a21082ed8e1c3a2d495f400ebd719fd43f08f65
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-15 20:54:08 +0600

    File updated at 15.12.2025, 20:54:05

--- 3774-AbsoluteDifferenceBetweenMaximumandMinimumKElements/3774-AbsoluteDifferenceBetweenMaximumandMinimumKElements.py
+++ 3774-AbsoluteDifferenceBetweenMaximumandMinimumKElements/3774-AbsoluteDifferenceBetweenMaximumandMinimumKElements.py
@@ -1,33 +1,35 @@
-# Last updated: 15.12.2025, 20:32:50
+# Last updated: 15.12.2025, 20:54:05
 1import random
 2
 3class Solution:
 4    def absDifference(self, nums: list[int], k: int) -> int:
 5        
 6        def get_sum_smallest_k(arr, k):
-7            if not arr: return 0
-8            
-9            pivot = random.choice(arr)
-10            
-11            left = [x for x in arr if x < pivot] 
-12            mid = [x for x in arr if x == pivot]  
-13            right = [x for x in arr if x > pivot] 
-14            
-15            len_l = len(left)
-16            len_m = len(mid)
-17            
-18            if k <= len_l:
-19                return get_sum_smallest_k(left, k)
-20            elif k <= len_l + len_m:
-21                return sum(left) + pivot * (k - len_l)
-22            else:
-23                return sum(left) + sum(mid) + get_sum_smallest_k(right, k - len_l - len_m)
-24        
-25        sum_min = get_sum_smallest_k(nums, k)
-26        
-27
-28        neg_nums = [-x for x in nums]
-29        sum_max_neg = get_sum_smallest_k(neg_nums, k)
-30        sum_max = -sum_max_neg
-31        
-32        return abs(sum_max - sum_min)
+7            if not arr: 
+8                return 0
+9
+10            pivot = random.choice(arr)
+11
+12            left = [ x for x in arr if x < pivot]
+13            mid = [x for x in arr if x == pivot]
+14            right = [ x for x in arr if x > pivot]
+15
+16            if len(left) >= k:
+17                if len(left) == k:
+18                    return sum(left)
+19                else:
+20                    return get_sum_smallest_k(left, k)
+21            elif len(left) + len(mid) >= k:
+22                if len(mid) + len(left) == k:
+23                    return sum(mid) + sum(left)
+24                else:
+25                    return sum(left) + pivot * (k - len(left))
+26            else:
+27                return sum(left) + sum(mid) + get_sum_smallest_k(right, k - len(left) - len(mid))\
+28        
+29        sum_left = get_sum_smallest_k(nums, k)
+30
+31        arr = [-x for x in nums]
+32        sum_right = -get_sum_smallest_k(arr, k)
+33
+34        return sum_right - sum_left
