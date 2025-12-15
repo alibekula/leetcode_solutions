f865d604ec8d8174488712081941fc998984f887
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-16 01:06:11 +0600

    File updated at 16.12.2025, 01:06:10

--- 740-DeleteandEarn/740-DeleteandEarn.py
+++ 740-DeleteandEarn/740-DeleteandEarn.py
@@ -1,4 +1,4 @@
-# Last updated: 16.12.2025, 00:21:58
+# Last updated: 16.12.2025, 01:06:10
 1
 2class Solution:
 3    def deleteAndEarn(self, nums: List[int]) -> int:
@@ -20,32 +20,31 @@
 19
 20        ranges = []
 21        
-22        sett = set(counter.keys())
-23
-24        for start in sett:
-25            if start-1 not in sett:
-26                end = start
-27
-28                while end + 1 in sett:
-29                    end += 1
-30                
-31                ranges.append([start, end])
-32            
-33
-34        for start, end in ranges:
-35            
-36            current_points = []
-37            
-38            for num_val in range(start, end + 1):
-39                current_points.append(num_val * counter[num_val])
-40                
-41            dp = [0] * len(current_points)
-42            dp[0] = current_points[0]
-43            dp[1] = max(current_points[0], current_points[1])
-44            
-45            for i in range(2, len(current_points)):
-46                dp[i] = max(dp[i - 1], dp[i - 2] + current_points[i])
-47            
-48            total += dp[-1]
-49        
-50        return total
+22
+23        for start in counter:
+24            if start-1 not in counter:
+25                end = start
+26
+27                while end + 1 in counter:
+28                    end += 1
+29                
+30                ranges.append([start, end])
+31            
+32
+33        for start, end in ranges:
+34            
+35            current_points = []
+36            
+37            for num_val in range(start, end + 1):
+38                current_points.append(num_val * counter[num_val])
+39                
+40            dp = [0] * len(current_points)
+41            dp[0] = current_points[0]
+42            dp[1] = max(current_points[0], current_points[1])
+43            
+44            for i in range(2, len(current_points)):
+45                dp[i] = max(dp[i - 1], dp[i - 2] + current_points[i])
+46            
+47            total += dp[-1]
+48        
+49        return total
