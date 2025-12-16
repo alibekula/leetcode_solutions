2a9a6b2fe44e2f87ef35053ae09e8532a747e542
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-16 17:56:06 +0600

    File updated at 16.12.2025, 17:56:05

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,23 +1,16 @@
-# Last updated: 13.08.2025, 17:00:45
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        dct = {}
-
-        for s in strs:
-            count = [0]*30
-
-            for char in s:
-                count[ord(char) - ord('a')] += 1
-
-            key = tuple(count)
-            
-            if key not in dct:
-                dct[key] = []
-            
-            dct[key].append(s)
-        
-        return list(dct.values())
-            
-
-        
+# Last updated: 16.12.2025, 17:56:05
+1class Solution:
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3        
+4        hashmap = {}
+5
+6        for s in strs:
+7            key = [0] * 30
+8            for char in s:
+9                key[ord(char) - ord('a')] += 1
+10            
+11            key = tuple(key)
+12            hashmap[key] = hashmap.get(key, [])
+13            hashmap[key].append(s)
+14        
+15        return list(hashmap.values())
