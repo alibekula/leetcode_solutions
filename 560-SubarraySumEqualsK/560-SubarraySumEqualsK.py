fb57a5814476ebfe8081aaf527bf12cd01c38851
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-16 17:41:25 +0600

    File updated at 16.12.2025, 17:41:25

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -1,21 +1,18 @@
-# Last updated: 13.08.2025, 16:57:16
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        dct = {0:1}
-        curr = 0
-        r = 0
-        total = 0
-
-        while r < len(nums):
-            curr += nums[r]
-
-            if k - curr in dct: 
-                total += dct[k-curr]
-            
-            if -curr not in dct:
-                dct[-curr] = 0
-            
-            dct[-curr] += 1
-            r += 1
-        
-        return total
+# Last updated: 16.12.2025, 17:41:25
+1class Solution:
+2    def subarraySum(self, nums: List[int], k: int) -> int:
+3        
+4        prefix = {0:1}
+5        curr = 0
+6        count = 0
+7        # k = 6 arr = 1 2 3 1 0 0 0 
+8
+9        for num in nums:
+10            curr += num
+11
+12            if curr - k in prefix:
+13                count += prefix[curr-k]
+14
+15            prefix[curr] = prefix.get(curr, 0) + 1
+16
+17        return count
