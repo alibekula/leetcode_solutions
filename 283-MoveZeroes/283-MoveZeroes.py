27f60c913c5e5d2c014219b238abae0c17088d5a
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-16 17:49:36 +0600

    File updated at 16.12.2025, 17:49:35

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,24 +1,24 @@
-# Last updated: 13.08.2025, 16:58:03
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        zeros = 0
-        nonzero_pos = 0
-        l = 0
-
-        while l < len(nums):
-            if nums[l] != 0:
-                nums[nonzero_pos] = nums[l]
-                nonzero_pos += 1
-            else:
-                zeros += 1
-            l += 1
-        
-        for j in range(nonzero_pos, len(nums)):
-            nums[j] = 0
-        
-        return nums
-
-        
+# Last updated: 16.12.2025, 17:49:35
+1class Solution:
+2    def moveZeroes(self, nums: List[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        zeros_count = 0
+7        p = 0
+8        n = len(nums)
+9        # 0 1 2 0 3 4 0 5 6
+10
+11        for i in range(n):
+12            if nums[i] == 0:
+13                zeros_count += 1
+14            else:
+15                nums[p] = nums[i]
+16                p += 1
+17        
+18        for j in range(n-1, n-zeros_count-1,-1):
+19            nums[j] = 0
+20
+21        
+22
+23        
