e09db1d2b0fb0165aaec7e14528e6897e6a47d05
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-16 21:19:11 +0600

    File updated at 16.12.2025, 21:19:11

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -1,25 +1,22 @@
-# Last updated: 10.10.2025, 22:50:11
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        
-        def dfs(l, r, path):
-            nonlocal n
-
-            if len(path) >= 2*n:
-                ans.append(''.join(path))
-                return 
-            
-            if l < n:
-                path.append('(')
-                dfs(l+1, r, path)
-                path.pop()
-            
-            if r < l:
-                path.append(')')
-                dfs(l, r+1, path)
-                path.pop()
-        
-        #  ( ( ( )))
-        ans = []
-        dfs(0, 0, [])
-        return ans
+# Last updated: 16.12.2025, 21:19:11
+1class Solution:
+2    def generateParenthesis(self, n: int) -> List[str]:
+3        ans = []
+4
+5        def dfs(l=0, r=0, s=''):
+6            nonlocal n
+7            if len(s) == n * 2:
+8                ans.append(s)
+9                return 
+10            
+11            if l < n:
+12                dfs(l+1, r, s+'(')
+13            
+14            if r < l:
+15                dfs(l, r+1, s+')')
+16        
+17        dfs()
+18
+19        return ans
+20
+21
