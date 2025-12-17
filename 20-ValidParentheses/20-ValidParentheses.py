ef9997ff06eed1ea2001cbb36187a8bc0a794abe
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-17 19:28:22 +0600

    File updated at 17.12.2025, 19:28:21

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,26 +1,20 @@
-# Last updated: 13.08.2025, 17:01:14
-class Solution:
-    def isValid(self, s: str) -> bool:
-
-        if not s:
-            return True 
-
-        dct = {')':'(', ']':'[', '}':'{'}
-        sett = set(['(', '[', '{'])
-        stack = []
-
-        for bracket in s:
-            if bracket in sett:
-                stack.append(bracket)
-            else:
-                if stack and stack[-1] != dct[bracket]:
-                    return False
-                                
-                if not stack:
-                    return False
-                stack.pop()
-        
-        return not stack
-
-
-        
+# Last updated: 17.12.2025, 19:28:21
+1class Solution:
+2    def isValid(self, s: str) -> bool:
+3        
+4        dct = {'(': ')',
+5                '{': '}',
+6                '[': ']'}
+7
+8        stack = []
+9
+10        for char in s:
+11            if char in dct:
+12                stack.append(char)
+13            else:
+14                if stack and dct[stack[-1]] == char:
+15                    stack.pop()
+16                else:
+17                    return False
+18        
+19        return True if not stack else False
