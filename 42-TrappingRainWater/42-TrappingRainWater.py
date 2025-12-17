4854c374d92673ab92b94519c98c271dc9877cb5
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-17 21:29:22 +0600

    File updated at 17.12.2025, 21:29:21

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,20 +1,26 @@
-# Last updated: 16.08.2025, 17:20:19
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        l, r = 0, len(height)-1
-        total = 0
-        left_max = height[l]
-        right_max = height[r]
-
-        while l <= r:
-
-            if left_max <= right_max:
-                left_max = max(height[l], left_max)
-                total += max(0, min(left_max, right_max) - height[l])
-                l += 1
-            else:
-                right_max = max(height[r], right_max)
-                total += max(0, min(left_max, right_max) - height[r])
-                r -= 1
-
-        return total
+# Last updated: 17.12.2025, 21:29:21
+1class Solution:
+2    def trap(self, height: List[int]) -> int:
+3        
+4        n = len(height)
+5        l, r = 0, n-1
+6        total = 0
+7        left_max, right_max = 0, 0
+8
+9        while l < r:
+10            left_height = height[l]
+11            right_height = height[r]
+12
+13            left_max = max(left_max, left_height)
+14            right_max = max(right_max, right_height)
+15            curr_min_max = min(left_max, right_max)
+16
+17            if left_height <= right_height:
+18                total += max(0, curr_min_max - left_height)
+19                l += 1
+20            else:
+21                total += max(0, curr_min_max - right_height)
+22                r -= 1
+23        
+24        return total
+25
