ce969b0ee3cf357022302ac9578758234b17837b
alibekula <146336542+alibekula@users.noreply.github.com>
2025-12-18 16:55:32 +0600

    File updated at 18.12.2025, 16:55:32

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,17 +1,26 @@
-# Last updated: 27.09.2025, 06:45:45
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        
-        total = 0
-        curr = 0
-        prefix = {0:1}
-
-        for num in nums:
-            curr += num
-
-            if curr - k in prefix:
-                total += prefix[curr-k]
-
-            prefix[curr] = prefix.get(curr, 0) + 1
-
-        return total
+# Last updated: 18.12.2025, 16:55:32
+1class Solution:
+2    def lengthOfLongestSubstring(self, s: str) -> int:
+3        # abcabc
+4        if len(s) == 0:
+5            return 0
+6
+7        sett = set(s[0])
+8        longest = 1
+9        n = len(s)
+10        l, r = 0, 1
+11
+12        while r < n:
+13
+14            while r < n and s[r] not in sett:
+15                sett.add(s[r])
+16                longest = max(longest, r-l+1)
+17                r += 1
+18            
+19            while r < n and l < r and s[r] in sett:
+20                sett.remove(s[l])
+21                l += 1
+22        
+23        return longest
+24
+25
