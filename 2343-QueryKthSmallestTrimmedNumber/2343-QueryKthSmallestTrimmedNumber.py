b300bf768c0c6a9f0707a9aeccc799a3efb1a8e7
alibekula <146336542+alibekula@users.noreply.github.com>
2026-01-22 00:51:13 +0600

    File updated at 22.01.2026, 00:51:13

--- 2343-QueryKthSmallestTrimmedNumber/2343-QueryKthSmallestTrimmedNumber.py
+++ 2343-QueryKthSmallestTrimmedNumber/2343-QueryKthSmallestTrimmedNumber.py
@@ -1,13 +1,13 @@
-# Last updated: 22.01.2026, 00:47:23
+# Last updated: 22.01.2026, 00:51:13
 1class Solution:
 2    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
 3        
-4        max_trim = max(queries, key=lambda x: x[1])[1]
-5        min_trim = min(queries, key=lambda x: x[1])[1]
+4        trim_values = set([trim for _, trim in queries])
+5        trim_values = sorted(trim_values)
 6        n = len(nums[0])
 7        dct = {}
 8
-9        for i in range(min_trim, max_trim + 1):  
+9        for i in trim_values:  
 10            curr = []
 11            for idx, num in enumerate(nums):
 12                curr.append([int(num[n-i:]), idx])
