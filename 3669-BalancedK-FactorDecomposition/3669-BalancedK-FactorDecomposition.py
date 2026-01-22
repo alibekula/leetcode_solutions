4177b1a68dd05c4c012c6ad0bdf8536a170d5055
alibekula <146336542+alibekula@users.noreply.github.com>
2026-01-23 00:09:37 +0600

    File updated at 23.01.2026, 00:09:36

--- 3669-BalancedK-FactorDecomposition/3669-BalancedK-FactorDecomposition.py
+++ 3669-BalancedK-FactorDecomposition/3669-BalancedK-FactorDecomposition.py
@@ -1,83 +1,18 @@
-# Last updated: 22.01.2026, 23:59:27
+# Last updated: 23.01.2026, 00:09:36
 1class Solution:
-2    def minDifference(self, n: int, k: int) -> List[int]:
-3
-4        tmp = n ** (1/k)
+2    def minDifference(self, n: int, k: int, best=float('inf')) -> List[int]:
+3        
+4        if k == 1: return [n]
 5
-6        if tmp == int(tmp):
-7            return [int(tmp) for _ in range(k)]
-8        
-9        def get_factors(num):
-10
-11            if num == 1:
-12                return [1]
+6        for cand in range(1, ceil(n**(1/k))+1):
+7
+8            if n % cand != 0: continue
+9
+10            divs = self.minDifference(n//cand, k-1)
+11            divs.append(cand)
+12            mn, mx = min(divs), max(divs)
 13
-14            factors = []
-15
-16            for i in range(2, int(num ** 0.5) + 1):
-17                
-18                if i > num:
-19                    break
-20
-21                factor = 1
-22                while num % i == 0:
-23                    num //= i
-24                    factors.append(i)
-25            
-26            if num != 1: factors.append(num)
-27
-28            return factors
-29        
-30        factors = get_factors(n)
-31
-32        while len(factors) < k:
-33            factors.append(1)
-34
-35        factors.sort()
-36
-37        if len(factors) == k:
-38            return factors
-39        
-40        buckets = [1 for _ in range(k)]
-41        min_diff = float('inf')
-42        best_buckets = []
-43
-44        def backtrack(idx):
-45            nonlocal min_diff, k, best_buckets
-46
-47            if idx == len(factors):
-48                curr_max_diff = max(buckets) - min(buckets)
-49
-50                if curr_max_diff < min_diff:
-51                    min_diff = curr_max_diff
-52                    best_buckets = buckets[:]
-53                
-54                return
-55            
-56            prime = factors[idx]
-57            seen = set()
-58
-59            for i in range(k):
-60
-61                if buckets[i] in seen:
-62                    continue
-63                
-64                seen.add(buckets[i])
-65
-66                buckets[i] = buckets[i] * prime
-67                backtrack(idx+1)
-68                buckets[i] //= prime
-69        
-70        backtrack(0)
-71
-72        return best_buckets
-73
-74
-75
-76
-77
-78
-79
-80        
-81
-82
+14            if best > mx-mn:
+15                best, ans = mx-mn, divs
+16        
+17        return ans
