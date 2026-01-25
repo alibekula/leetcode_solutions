d1e3517cba9edf857ba73ff7be3b3778199d5de7
alibekula <146336542+alibekula@users.noreply.github.com>
2026-01-26 03:48:40 +0600

    File updated at 26.01.2026, 03:48:40

--- 997-FindtheTownJudge/997-FindtheTownJudge.py
+++ 997-FindtheTownJudge/997-FindtheTownJudge.py
@@ -1,23 +1,18 @@
-# Last updated: 26.01.2026, 03:42:39
+# Last updated: 26.01.2026, 03:48:40
 1class Solution:
 2    def findJudge(self, n: int, trust: List[List[int]]) -> int:
 3
-4        candidate = -1
-5        graph = {i: set() for i in range(1, n+1)}
-6
-7        for u, v in trust:
-8            graph[u].add(v)
-9
-10        for node in graph:
-11            if not graph[node]:
-12                if candidate != -1:
-13                    return -1
-14                else:
-15                    candidate = node
-16
-17        for node in graph:
-18            if node != candidate and candidate not in graph[node]:
-19                return -1
-20        
-21        return candidate
-22
+4        in_degree = {i: [0, 0] for i in range(1, n+1)}
+5
+6        for u, v in trust:
+7            in_degree[u][0] += 1
+8            in_degree[v][1] += 1
+9        
+10        for node in in_degree:
+11            i, j = in_degree[node]
+12
+13            if i == 0 and j == n-1:
+14                return node
+15        
+16        return -1
+17
