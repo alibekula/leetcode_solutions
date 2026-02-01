d04dc8ef4421d730b3c4e2b8719f087c7bf25b61
alibekula <146336542+alibekula@users.noreply.github.com>
2026-02-01 17:47:24 +0600

    File updated at 01.02.2026, 17:47:24

--- 3010-DivideanArrayIntoSubarraysWithMinimumCostI/3010-DivideanArrayIntoSubarraysWithMinimumCostI.py
+++ 3010-DivideanArrayIntoSubarraysWithMinimumCostI/3010-DivideanArrayIntoSubarraysWithMinimumCostI.py
@@ -1,13 +1,14 @@
-# Last updated: 01.02.2026, 17:40:17
+# Last updated: 01.02.2026, 17:47:24
 1class Solution:
 2    def minimumCost(self, nums: List[int]) -> int:
-3        a, b, c = nums[0], min(nums[1], nums[2]), max(nums[1], nums[2])
-4        n = len(nums)
-5        # 1 5 100 4 3 
-6        for i in range(3, n):
-7            if nums[i] < b:
-8                b, c = nums[i], b
-9            elif nums[i] < c:
-10                c = nums[i]
-11        
-12        return a + b + c
+3        min1 = float('inf')
+4        min2 = float('inf')
+5        
+6        for x in nums[1:]:
+7            if x < min1:
+8                min2 = min1
+9                min1 = x
+10            elif x < min2:
+11                min2 = x
+12                
+13        return nums[0] + min1 + min2
